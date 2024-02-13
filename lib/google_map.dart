5f2f3ad77868223dd2b39f798c613a59154ef0e5
Vasco Gomes <vasco.gomes2000@gmail.com>
2024-02-13 00:58:36 +0000

    Update

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -14,7 +14,14 @@ void main() {
 const String google_api_key = "AIzaSyCsgQMdy2MoeXZYKAm7VOr_ZKPlvZ2jXxA";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Values Passed By Parameter from Google Maps
+String parkingName = "Estacionamento Parque das Nações";
+const List<int> parkNumbersList = [1, 5, 10];
+int selectedItem = 1;
+int parkingNumbersListSize = parkNumbersList.length;
+bool paidPark = false;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class GoogleMapActivity extends StatefulWidget {
   const GoogleMapActivity({Key? key}) : super(key: key);
 
@@ -26,7 +33,7 @@ class GoogleMapState extends State<GoogleMapActivity> {
   
   final Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng sourceLocation = LatLng(37.32291173371677, -122.01412296938393);
   static const LatLng destinationHandicapNormal = LatLng(37.33429383, -122.06600055);
   static const LatLng destinationHandicapFull = LatLng(37.33429384, -122.06600055);
   static const LatLng destinationEmpty = LatLng(37.33429383, -122.06600056);
@@ -42,11 +49,6 @@ class GoogleMapState extends State<GoogleMapActivity> {
   void getCurrentLocation(){
 
     Location location = Location();
-
-    /*location.getLocation().then((location) => {
-      currentLocation = location
-    });*/
-
   }
 
   void getPolyPoints() async {
@@ -70,7 +72,12 @@ class GoogleMapState extends State<GoogleMapActivity> {
 
   }
 
-  void setCustomMarkerIcon(){
+  Future<void> setCustomMarkerIcon() async {
+
+    var markerIcon = await BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(),
+      'assets/handicap_green.png'
+    );
 
     BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, "assets/handicap.png")
     .then((icon) => {
@@ -98,6 +105,10 @@ class GoogleMapState extends State<GoogleMapActivity> {
 
   }
 
+  void reserveParkingSpot(){
+
+  }
+
   @override
   void initState(){
 
@@ -111,9 +122,30 @@ class GoogleMapState extends State<GoogleMapActivity> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
             
-            body: GoogleMap(
+            body: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Stack( 
+
+                
+
+              children:[
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Origem",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                  )
+                ),
+              ),  
+                
+              GoogleMap(
               initialCameraPosition: 
               const CameraPosition(
                 target: sourceLocation,
@@ -121,6 +153,7 @@ class GoogleMapState extends State<GoogleMapActivity> {
               ),
 
               polylines: {
+                //https://youtu.be/B9hsWOCXb_o
                 Polyline(
                   polylineId: const PolylineId("route"),
                   points: polylineCoordinates,
@@ -130,13 +163,12 @@ class GoogleMapState extends State<GoogleMapActivity> {
               },
 
               markers: {
+                //https://youtu.be/B9hsWOCXb_o
                 Marker(
                   icon: currentLocationIcon,
                   markerId: const MarkerId("source"),
                   position: sourceLocation,
-                  onTap: () {
-                    const Text("Ganda Tap");
-                  },
+                  
                 ),
 
                 //Destination 1 - Park Normal
@@ -144,10 +176,78 @@ class GoogleMapState extends State<GoogleMapActivity> {
                   icon: destinationHandicapNormalIcon,
                   markerId: const MarkerId("destination1"),
                   position: destinationHandicapNormal,
+                  onTap: () {
+                    //https://www.youtube.com/watch?v=4pn-_md5Ol4
+                    showDialog(
+                      context: context,
+                      builder: (context) => 
+                      SizedBox(
+                        width: 30.0,
+                        height: 250.0,
+                        child: AlertDialog(
+                        title: Text(parkingName),
+                        content: Column(
+                          children: [
+                            //One Row for each Item inside the Pop-Up
+                            Row(
+                              children: [
+                                Text("Lugares de Necessidades Especiais: "),
+                                Text('$parkingNumbersListSize'),
+                              ],
+                            ),
+
+                            Row(
+                              children: [
+                                Text("Número do Lugar: "),
+                                DropdownButton<int>(
+                                  //https://youtu.be/GHkwpepeLoE
+                                  value: selectedItem,
+                                  onChanged: (int? newValue) {
+                                    if (newValue != null) {
+                                      // Update the selected item when the user makes a selection
+                                      selectedItem = newValue;
+                                    }
+                                  },
+                                  items: parkNumbersList.map((int item) {
+                                    return DropdownMenuItem<int>(
+                                      value: item,
+                                      child: Text('$item'),
+                                    );
+                                  }).toList(),
+                                ),
+                              ],
+                            ),
+
+                            Row(
+                              children: [
+                                Text("Pago: "),
+                                Text(paidPark ? 'Sim' : 'Não'),
+                              ],
+                            )
+                            
+                          ]
+                          ),
+                        actions: [
+                          TextButton(
+                            child: Text("Reservar Lugar"), 
+                            onPressed: reserveParkingSpot,
+                          ),
+
+                          TextButton(
+                            child: Text("Cancelar"), 
+                            onPressed: () => {
+                              Navigator.pop(context)
+                            },
+                          )
+                        ],
+                        )
+                      )
+                    );
+                  },
                 ),
 
                 //Destination - Almost Full Handicap Parking
-                Marker(
+                /*Marker(
                   icon: destinationHandicapFullIcon,
                   markerId: const MarkerId("destination2"),
                   position: destinationHandicapFull,
@@ -158,9 +258,41 @@ class GoogleMapState extends State<GoogleMapActivity> {
                   icon: destinationHandicapEmptyIcon,
                   markerId: const MarkerId("destination3"),
                   position: destinationEmpty,
-                )
+                )*/
               },
             ),
-            );
+            
+            /*Positioned(
+              top: 16.0,
+              left: 16.0,
+              right: 16.0,
+              child: Row(children: [
+                
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Origem",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                  )
+                ),
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Destino",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                  )
+                ),
+              ),
+            ],))*/
+
+            ],
+            ),
+            ));
   }
 }
